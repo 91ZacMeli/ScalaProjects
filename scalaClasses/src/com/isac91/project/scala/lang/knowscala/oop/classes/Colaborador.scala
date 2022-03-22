ccd9675de29e50eac4ad4a880e99569d73e11302
MyMeliMail <isac.cordeiro@mercadolivre.com>
2022-03-22 13:07:16 -0300

    Exemple of simple class

---
+++ scalaClasses/src/com/isac91/project/scala/lang/knowscala/oop/classes/Colaborador.scala
@@ -0,0 +1,6 @@
+package com.isac91.project.scala.lang.knowscala.oop.classes
+
+class Colaborador {
+  def mensagem(nome: String): String = s"Seja bem vindo(a) $nome a nossa empresa"
+  def descricao() = println("Esta eh a nossa primeira classe em Scala")
+}
